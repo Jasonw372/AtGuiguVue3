0a6f28f6d2794feeb8b9a84d42d750cff244a5de
Jason372 <1174601344@qq.com>
2024-02-03 03:10:00 +0800

    feat: Add login form rules

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,6 +5,7 @@ import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElNotification } from 'element-plus'
 import { getTime } from '@/utils/time'
+import type { ElForm } from 'element-plus/lib/components/index.js'
 
 const $router = useRouter()
 
@@ -17,10 +18,15 @@ const loginForm = reactive<{
   username: '',
   password: '',
 })
+let loginFormRef = ref<InstanceType<typeof ElForm>>()
+
 const userStore = useUserStore()
 
 const login = async () => {
   buttonLoading.value = true
+
+  loginFormRef.value?.validate()
+
   try {
     await userStore.userLogin(loginForm)
     $router.push({ path: '/' })
@@ -29,18 +35,55 @@ const login = async () => {
       type: 'success',
       title: `HI,${getTime()}好`,
       message: '欢迎回来',
-
     })
-
+    buttonLoading.value = false
 
   } catch (error) {
     ElNotification({
       type: 'error',
       message: (error as Error).message,
     })
+    buttonLoading.value = false
   }
+}
 
-  buttonLoading.value = false
+//自定义校验规则函数
+const validatorUserName = (rule: any, value: any, callback: any) => {
+  //rule:即为校验规则对象
+  //value:即为表单元素文本内容
+  //函数:如果符合条件callBack放行通过即为
+  //如果不符合条件callBack方法,注入错误提示信息
+  if (value.length >= 5) {
+    callback()
+  } else {
+    callback(new Error('账号长度至少五位'))
+  }
+}
+
+const validatorPassword = (rule: any, value: any, callback: any) => {
+  if (value.length >= 6) {
+    callback()
+  } else {
+    callback(new Error('密码长度至少六位'))
+  }
+}
+
+//定义表单校验需要配置对象
+const rules = {
+  //规则对象属性:
+  //required,代表这个字段务必要校验的
+  //min:文本长度至少多少位
+  //max:文本长度最多多少位
+  //message:错误的提示信息
+  //trigger:触发校验表单的时机 change->文本发生变化触发校验,blur:失去焦点的时候触发校验规则
+  username: [
+    // { required: true, min: 6, max: 10, message: '账号长度至少六位', trigger: 'change' }
+    { trigger: 'change', validator: validatorUserName },
+  ],
+  password: [
+    // { required: true, min: 6, max: 15, message: '密码长度至少6位', trigger: 'change' }
+    { trigger: 'change', validator: validatorPassword },
+  ],
 }
 </script>
 
@@ -49,11 +92,11 @@ const login = async () => {
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12">
-        <el-form class="login-form" :model="loginForm">
+        <el-form class="login-form" :model="loginForm" :ref="loginFormRef" :rules="rules">
           <h1>登录</h1>
           <h2>欢迎来到硅谷甄选</h2>
 
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input
               type="text"
               :prefix-icon="User"
@@ -61,7 +104,7 @@ const login = async () => {
             ></el-input>
           </el-form-item>
 
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               type="password"
               :prefix-icon="Lock"
